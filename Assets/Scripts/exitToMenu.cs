c2a894f209d74d29bf4beb48d89acd890b3f2252
Kent <unconfigured@null.spigotmc.org>
2020-02-12 16:11:00 +0800

    bunch of changes
    yeye

---
+++ Assets/Scripts/exitToMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class exitToMenu : MonoBehaviour {
+
+    public Button myButton;
+    void OnEnable()
+    {
+        myButton.onClick.AddListener(Button);
+    }
+
+    void Button()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+
+
+}
